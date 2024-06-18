3f4bf2abe7da8fb32966ab615f09b5e452099831
Tiago <ti.almeida58@gmail.com>
2024-06-18 11:03:08 -0300

    raw: first commit

---
+++ src/test/java/br/com/hrbackend/HrBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.hrbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
