43bf9ae782e6bf3e7a648bf15c6ac3cf26b3c72b
Tiago <ti.almeida58@gmail.com>
2024-06-18 13:36:36 -0300

    raw: creating sql script to create licenca table

---
+++ src/main/resources/db/migration/V8__create-licenca.sql
@@ -0,0 +1,16 @@
+CREATE TABLE licenca (
+    idlicenca INT AUTO_INCREMENT,
+    tipo VARCHAR(45),
+    descricao VARCHAR(45),
+    data_inicio DATE,
+    data_fim DATE,
+    retorno DATE,
+    funcionario_nome VARCHAR(45),
+    idfuncionario INT,
+    setor VARCHAR(45),
+    filial INT,
+    idlembrete INT,
+    funcionario_idfuncionario INT,
+    PRIMARY KEY (idlicenca),
+    FOREIGN KEY (funcionario_idfuncionario) REFERENCES funcionario(idfuncionario)
+);
