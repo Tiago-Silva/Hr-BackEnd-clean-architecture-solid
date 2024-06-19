f75c5020dbd1bc0603036d336667dfe22da3d295
Tiago <ti.almeida58@gmail.com>
2024-06-19 13:50:21 -0300

    feat: creating entity Documentos.java

---
+++ src/main/java/br/com/hrbackend/entity/Documentos.java
@@ -0,0 +1,48 @@
+package br.com.hrbackend.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.io.Serializable;
+import java.sql.Date;
+
+@Entity
+@Table(name = "documentos")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "iddocumentos")
+public class Documentos implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int iddocumentos;
+
+    private String tipo;
+
+    @Column(name = "url_documento")
+    private String urlDocumento;
+
+    private String nome;
+
+    private String descricao;
+
+    private int idfuncionario;
+
+    @Column(name = "funcionario_nome")
+    private String funcionarioNome;
+
+    private String departamento;
+
+    private int empresa;
+
+    private Date data;
+
+    private int idfalta;
+
+    @ManyToOne
+    @JoinColumn(name = "funcionario_idfuncionario")
+    private Funcionario funcionario;
+}
