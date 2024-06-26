6109d110bda7c4df2032e22ee9cf1101c2e67570
Tiago <ti.almeida58@gmail.com>
2024-06-26 16:45:05 -0300

    feat: creating class entity the domain

---
+++ src/main/java/br/com/hrbackend/domain/entity/Documentos.java
@@ -0,0 +1,39 @@
+package br.com.hrbackend.domain.entity;
+
+import lombok.*;
+
+import java.io.Serializable;
+import java.sql.Date;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "iddocumentos")
+public class Documentos implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private int iddocumentos;
+
+    private String tipo;
+
+    private String urlDocumento;
+
+    private String nome;
+
+    private String descricao;
+
+    private int idfuncionario;
+
+    private String funcionarioNome;
+
+    private String departamento;
+
+    private int empresa;
+
+    private Date data;
+
+    private int idfalta;
+
+    private Funcionario funcionario;
+}
