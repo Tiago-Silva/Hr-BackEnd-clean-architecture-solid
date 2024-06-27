972651991a319d12db13fac4385ed83f048b47ab
Tiago <ti.almeida58@gmail.com>
2024-06-27 09:04:30 -0300

    feat: creating DTO to domain class Empresa

---
+++ src/main/java/br/com/hrbackend/application/dto/EmpresaResponseDTO.java
@@ -0,0 +1,20 @@
+package br.com.hrbackend.application.dto;
+
+public record EmpresaResponseDTO(
+        int idempresa,
+        String nomeFantasia,
+        String razaoSocial,
+        String cnpj,
+        String inscricaoEstadual,
+        String bairro,
+        String cidade,
+        String endereco,
+        String estado,
+        String telefone,
+        String logoMarca,
+        boolean filial,
+        int empresaController,
+        boolean matriz,
+        String status
+) {
+}
