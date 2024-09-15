e13cd8830bedec0d8c3b2197b070f7748ec99a08
Tiago <ti.almeida58@gmail.com>
2024-09-15 13:55:47 -0300

    feat: creating user intents DeleteEmpressUseCase

---
+++ src/main/java/br/com/hrbackend/application/usecases/Empress/DeleteEmpressUseCase.java
@@ -0,0 +1,21 @@
+package br.com.hrbackend.application.usecases.Empress;
+
+import br.com.hrbackend.application.gateway.EmpressGateway;
+import br.com.hrbackend.application.usecases.UseCase;
+import br.com.hrbackend.domain.entity.Empress;
+
+public class DeleteEmpressUseCase implements UseCase<Empress, Empress> {
+
+    private final EmpressGateway empressGateway;
+    public DeleteEmpressUseCase(EmpressGateway empressGateway) {
+        this.empressGateway = empressGateway;
+    }
+
+    @Override
+    public Empress execute(Empress empress) {
+        if (empress == null || empress.getIdempresa() <= 0) {
+            throw new IllegalArgumentException("Invalid arguments");
+        }
+        return this.empressGateway.deleteEmpress(empress);
+    }
+}
