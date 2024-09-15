0fa1bfb18a9180fce95c015e052b2077e51f725b
Tiago <ti.almeida58@gmail.com>
2024-09-15 14:07:45 -0300

    feate: creating user intents UpdateUserUseCase

---
+++ src/main/java/br/com/hrbackend/application/usecases/User/UpdateUserUseCase.java
@@ -0,0 +1,21 @@
+package br.com.hrbackend.application.usecases.User;
+
+import br.com.hrbackend.application.gateway.UserGateway;
+import br.com.hrbackend.application.usecases.UseCase;
+import br.com.hrbackend.domain.entity.Usuario;
+
+public class UpdateUserUseCase implements UseCase<Usuario, Usuario> {
+
+    private final UserGateway userGateway;
+    public UpdateUserUseCase(UserGateway userGateway) {
+        this.userGateway = userGateway;
+    }
+
+    @Override
+    public Usuario execute(Usuario usuario) {
+        if (usuario == null || usuario.getIdusuario() <= 0) {
+            throw new IllegalArgumentException("Invalid arguments");
+        }
+        return this.userGateway.updateUser(usuario);
+    }
+}
