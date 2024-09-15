87a7e468b87175e0e03ddea0de51b0d65f634288
Tiago <ti.almeida58@gmail.com>
2024-09-15 14:08:12 -0300

    feat: creating user intents DeleteUserUseCase

---
+++ src/main/java/br/com/hrbackend/application/usecases/User/DeleteUserUseCase.java
@@ -0,0 +1,22 @@
+package br.com.hrbackend.application.usecases.User;
+
+import br.com.hrbackend.application.gateway.UserGateway;
+import br.com.hrbackend.application.usecases.UseCase;
+import br.com.hrbackend.domain.entity.Usuario;
+
+public class DeleteUserUseCase implements UseCase<Usuario, Void> {
+
+    private final UserGateway userGateway;
+    public DeleteUserUseCase(UserGateway userGateway) {
+        this.userGateway = userGateway;
+    }
+
+    @Override
+    public Void execute(Usuario usuario) {
+        if (usuario == null || usuario.getIdusuario() <= 0) {
+            throw new IllegalArgumentException("Invalid arguments");
+        }
+        this.userGateway.deleteUser(usuario);
+        return null;
+    }
+}
