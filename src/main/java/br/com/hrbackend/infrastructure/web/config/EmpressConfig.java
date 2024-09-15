52abfbf4a363f3d44be00b36ed0403152651738b
Tiago <ti.almeida58@gmail.com>
2024-09-15 17:24:08 -0300

    feat: creating beans for empress use

--- src/main/java/br/com/hrbackend/infrastructure/web/config/EmpressConfig.java
+++ src/main/java/br/com/hrbackend/infrastructure/web/config/EmpressConfig.java
@@ -1,7 +1,7 @@
 package br.com.hrbackend.infrastructure.web.config;
 
 import br.com.hrbackend.application.gateway.EmpressGateway;
-import br.com.hrbackend.application.usecases.EmpressInteract;
+import br.com.hrbackend.application.usecases.Empress.*;
 import br.com.hrbackend.infrastructure.core.gateway.EmpressRepositoryGateway;
 import br.com.hrbackend.infrastructure.core.mapper.EmpressMapper;
 import br.com.hrbackend.infrastructure.data.repository.EmpressRepository;
@@ -12,8 +12,28 @@ import org.springframework.context.annotation.Configuration;
 public class EmpressConfig {
 
     @Bean
-    EmpressInteract empressInteract(EmpressGateway empressGateway) {
-        return new EmpressInteract(empressGateway);
+    CreateEmpressUseCase createEmpressUseCase(EmpressGateway empressGateway) {
+        return new CreateEmpressUseCase(empressGateway);
+    }
+
+    @Bean
+    UpdateEmpressUseCase updateEmpressUseCase(EmpressGateway empressGateway) {
+        return new UpdateEmpressUseCase(empressGateway);
+    }
+
+    @Bean
+    DeleteEmpressUseCase deleteEmpressUseCase(EmpressGateway empressGateway) {
+        return new DeleteEmpressUseCase(empressGateway);
+    }
+
+    @Bean
+    GetEmpressByIdUseCase getEmpressByIdUseCase(EmpressGateway empressGateway) {
+        return new GetEmpressByIdUseCase(empressGateway);
+    }
+
+    @Bean
+    GetBranchesByIdEmpressControllerUseCase getBranchesByIdEmpressControllerUseCase(EmpressGateway empressGateway) {
+        return new GetBranchesByIdEmpressControllerUseCase(empressGateway);
     }
 
     @Bean
