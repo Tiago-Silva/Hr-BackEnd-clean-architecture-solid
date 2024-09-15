b13bf1f6d5fba7da54eb13d9066e12635cc7d71a
Tiago <ti.almeida58@gmail.com>
2024-09-15 13:57:41 -0300

    feat: creating user intents GetPeriodByIdUseCase

---
+++ src/main/java/br/com/hrbackend/application/usecases/Period/GetPeriodByIdUseCase.java
@@ -0,0 +1,21 @@
+package br.com.hrbackend.application.usecases.Period;
+
+import br.com.hrbackend.application.gateway.PeriodGateway;
+import br.com.hrbackend.application.usecases.UseCase;
+import br.com.hrbackend.domain.entity.Periodo;
+
+public class GetPeriodByIdUseCase implements UseCase<Integer, Periodo> {
+
+    private final PeriodGateway periodGateway;
+    public GetPeriodByIdUseCase(PeriodGateway periodGateway) {
+        this.periodGateway = periodGateway;
+    }
+
+    @Override
+    public Periodo execute(Integer id) {
+        if (id <= 0) {
+            throw new IllegalArgumentException("Invalid arguments");
+        }
+        return this.periodGateway.getPeriodById(id);
+    }
+}
