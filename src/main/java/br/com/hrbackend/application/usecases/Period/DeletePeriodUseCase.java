7aa590c7480c1cea639e0ffadac432e862839993
Tiago <ti.almeida58@gmail.com>
2024-09-15 13:57:24 -0300

    feat: creating user intents DeletePeriodUseCase

---
+++ src/main/java/br/com/hrbackend/application/usecases/Period/DeletePeriodUseCase.java
@@ -0,0 +1,22 @@
+package br.com.hrbackend.application.usecases.Period;
+
+import br.com.hrbackend.application.gateway.PeriodGateway;
+import br.com.hrbackend.application.usecases.UseCase;
+import br.com.hrbackend.domain.entity.Periodo;
+
+public class DeletePeriodUseCase implements UseCase<Periodo, Void> {
+
+    private final PeriodGateway periodGateway;
+    public DeletePeriodUseCase(PeriodGateway periodGateway) {
+        this.periodGateway = periodGateway;
+    }
+
+    @Override
+    public Void execute(Periodo periodo) {
+        if (periodo == null || periodo.getIdperiodo() <= 0) {
+            throw new IllegalArgumentException("Invalid arguments");
+        }
+        this.periodGateway.deletePeriod(periodo);
+        return null;
+    }
+}
