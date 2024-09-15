deb4ef49f30d89eabb4676a2b4030e1a99846f00
Tiago <ti.almeida58@gmail.com>
2024-09-15 17:24:43 -0300

    refactor: changing to use the new use cases

--- src/main/java/br/com/hrbackend/infrastructure/web/controller/EmpressController.java
+++ src/main/java/br/com/hrbackend/infrastructure/web/controller/EmpressController.java
@@ -1,6 +1,6 @@
 package br.com.hrbackend.infrastructure.web.controller;
 
-import br.com.hrbackend.application.usecases.EmpressInteract;
+import br.com.hrbackend.application.usecases.Empress.*;
 import br.com.hrbackend.domain.entity.Empress;
 import br.com.hrbackend.infrastructure.web.dto.EmpresaRequestDTO;
 import br.com.hrbackend.infrastructure.web.dto.EmpresaResponseDTO;
@@ -15,59 +15,53 @@ import java.util.List;
 @RequestMapping("/empress")
 public class EmpressController {
 
-    private final EmpressInteract empressInteract;
+    private final CreateEmpressUseCase createEmpressUseCase;
+    private final UpdateEmpressUseCase updateEmpressUseCase;
+    private final DeleteEmpressUseCase deleteEmpressUseCase;
+    private final GetEmpressByIdUseCase getEmpressByIdUseCase;
+    private final GetBranchesByIdEmpressControllerUseCase getBranchesByIdEmpressControllerUseCase;
     private final EmpressMapper mapper;
-    public EmpressController(EmpressInteract empressInteract, EmpressMapper mapper) {
-        this.empressInteract = empressInteract;
+    public EmpressController(CreateEmpressUseCase createEmpressUseCase, UpdateEmpressUseCase updateEmpressUseCase, DeleteEmpressUseCase deleteEmpressUseCase, GetEmpressByIdUseCase getEmpressByIdUseCase, GetBranchesByIdEmpressControllerUseCase getBranchesByIdEmpressControllerUseCase, EmpressMapper mapper) {
+        this.createEmpressUseCase = createEmpressUseCase;
+        this.updateEmpressUseCase = updateEmpressUseCase;
+        this.deleteEmpressUseCase = deleteEmpressUseCase;
+        this.getEmpressByIdUseCase = getEmpressByIdUseCase;
+        this.getBranchesByIdEmpressControllerUseCase = getBranchesByIdEmpressControllerUseCase;
         this.mapper = mapper;
     }
 
     @PostMapping
     public ResponseEntity createEmpress(@RequestBody EmpresaRequestDTO requestDTO) {
         Empress empress = this.mapper.requestDTOToDomainObject(requestDTO);
-        this.empressInteract.createEmpress(empress);
+        this.createEmpressUseCase.execute(empress);
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
     @PutMapping
     public ResponseEntity updateEmpress(@RequestBody EmpresaResponseDTO responseDTO) {
         Empress empress = this.mapper.responseDTOToDomainObject(responseDTO);
-        this.empressInteract.updateEmpress(empress);
+        this.updateEmpressUseCase.execute(empress);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @DeleteMapping
     public ResponseEntity deleteEmpress(@RequestBody EmpresaResponseDTO responseDTO) {
         Empress empress = this.mapper.responseDTOToDomainObject(responseDTO);
-        this.empressInteract.deleteEmpress(empress);
+        this.deleteEmpressUseCase.execute(empress);
         return new ResponseEntity<>(HttpStatus.OK);
-    }
-
-    @GetMapping("/all")
-    public ResponseEntity<List<EmpresaResponseDTO>> getAllEmpress() {
-        return ResponseEntity.ok(
-            this.empressInteract.getAllEmpress().stream().map(this.mapper::domainObjecToResponseDTO).toList()
-        );
     }
 
     @GetMapping("/id/{idempresa}")
     public ResponseEntity<EmpresaResponseDTO> getEmpressById(@PathVariable("idempresa") int idempresa) {
         return ResponseEntity.ok(
-            this.mapper.domainObjecToResponseDTO(this.empressInteract.getEmpressById(idempresa))
+            this.mapper.domainObjecToResponseDTO(this.getEmpressByIdUseCase.execute(idempresa))
         );
     }
 
-    @GetMapping("/controllers")
-    public ResponseEntity<List<EmpresaResponseDTO>> getEmpressController() {
+    @GetMapping("/filiais/{idempress}")
+    public ResponseEntity<List<EmpresaResponseDTO>> getFiliais(@PathVariable("idempress") int idempress) {
         return ResponseEntity.ok(
-                this.empressInteract.getEmpressController().stream().map(this.mapper::domainObjecToResponseDTO).toList()
-        );
-    }
-
-    @GetMapping("/filiais")
-    public ResponseEntity<List<EmpresaResponseDTO>> getFiliais() {
-        return ResponseEntity.ok(
-                this.empressInteract.getEmpressFilial().stream().map(this.mapper::domainObjecToResponseDTO).toList()
+                this.getBranchesByIdEmpressControllerUseCase.execute(idempress).stream().map(this.mapper::domainObjecToResponseDTO).toList()
         );
     }
 }
