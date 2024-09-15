3031142b5879479374b8c0e0ee0929ed49c636ec
Tiago <ti.almeida58@gmail.com>
2024-09-15 13:56:43 -0300

    feat: creating user intents GetEmpressByIdUseCase

---
+++ src/main/java/br/com/hrbackend/application/usecases/Empress/GetEmpressByIdUseCase.java
@@ -0,0 +1,21 @@
+package br.com.hrbackend.application.usecases.Empress;
+
+import br.com.hrbackend.application.gateway.EmpressGateway;
+import br.com.hrbackend.application.usecases.UseCase;
+import br.com.hrbackend.domain.entity.Empress;
+
+public class GetEmpressByIdUseCase implements UseCase<Integer, Empress> {
+
+    private final EmpressGateway empressGateway;
+    public GetEmpressByIdUseCase(EmpressGateway empressGateway) {
+        this.empressGateway = empressGateway;
+    }
+
+    @Override
+    public Empress execute(Integer id) {
+        if (id == null || id <= 0) {
+            throw new IllegalArgumentException("Invalid arguments");
+        }
+        return this.empressGateway.getEmpressById(id);
+    }
+}
