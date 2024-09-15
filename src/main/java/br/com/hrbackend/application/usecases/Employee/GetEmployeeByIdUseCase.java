941da6958f4527a6b427b239484da07f1b9c3ca6
Tiago <ti.almeida58@gmail.com>
2024-09-15 13:10:36 -0300

    feat: creating user intention GetEmployeeByIdUseCase

---
+++ src/main/java/br/com/hrbackend/application/usecases/Employee/GetEmployeeByIdUseCase.java
@@ -0,0 +1,21 @@
+package br.com.hrbackend.application.usecases.Employee;
+
+import br.com.hrbackend.application.gateway.EmployeeGateway;
+import br.com.hrbackend.application.usecases.UseCase;
+import br.com.hrbackend.domain.entity.Employee;
+
+public class GetEmployeeByIdUseCase implements UseCase<Integer, Employee> {
+
+    private final EmployeeGateway employeeGateway;
+    public GetEmployeeByIdUseCase(EmployeeGateway employeeGateway) {
+        this.employeeGateway = employeeGateway;
+    }
+
+    @Override
+    public Employee execute(Integer id) {
+        if (id <= 0) {
+            throw new IllegalArgumentException("Invalid arguments");
+        }
+        return this.employeeGateway.getEmployeeById(id);
+    }
+}
