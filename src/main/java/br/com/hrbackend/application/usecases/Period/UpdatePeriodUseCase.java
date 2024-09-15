2293516f06b15063ee6302af34d5648a82772cf2
Tiago <ti.almeida58@gmail.com>
2024-09-15 13:57:14 -0300

    feat: creating user intents UpdatePeriodUseCase

---
+++ src/main/java/br/com/hrbackend/application/usecases/Period/UpdatePeriodUseCase.java
@@ -0,0 +1,21 @@
+package br.com.hrbackend.application.usecases.Period;
+
+import br.com.hrbackend.application.gateway.PeriodGateway;
+import br.com.hrbackend.application.usecases.UseCase;
+import br.com.hrbackend.domain.entity.Periodo;
+
+public class UpdatePeriodUseCase implements UseCase<Periodo, Periodo> {
+
+    private final PeriodGateway periodGateway;
+    public UpdatePeriodUseCase(PeriodGateway periodGateway) {
+        this.periodGateway = periodGateway;
+    }
+
+    @Override
+    public Periodo execute(Periodo periodo) {
+        if (periodo == null || periodo.getIdperiodo() <= 0) {
+            throw new IllegalArgumentException("Invalid arguments");
+        }
+        return this.periodGateway.updatePeriod(periodo);
+    }
+}
