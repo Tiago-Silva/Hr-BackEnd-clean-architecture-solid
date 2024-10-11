0db88d4e0844758f28b387c207addb4886b0ff28
Tiago <ti.almeida58@gmail.com>
2024-10-11 19:23:28 -0300

    refactor: using generic gateway

--- src/main/java/br/com/hrbackend/application/gateway/UserGateway.java
+++ src/main/java/br/com/hrbackend/application/gateway/UserGateway.java
@@ -3,10 +3,4 @@ package br.com.hrbackend.application.gateway;
 import br.com.hrbackend.domain.entity.Usuario;
 
 
-public interface UserGateway {
-
-    Usuario createUser(Usuario usuario);
-    Usuario updateUser(Usuario usuario);
-    void deleteUser(Usuario usuario);
-    Usuario getUserById(int id);
-}
+public interface UserGateway extends Gateway<Usuario> {}
