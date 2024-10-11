1ef97da4c69e7635e6067128a8f70fcacc993cc9
Tiago <ti.almeida58@gmail.com>
2024-10-11 19:18:04 -0300

    refactor: using generic interface

--- src/main/java/br/com/hrbackend/application/gateway/PeriodGateway.java
+++ src/main/java/br/com/hrbackend/application/gateway/PeriodGateway.java
@@ -2,10 +2,4 @@ package br.com.hrbackend.application.gateway;
 
 import br.com.hrbackend.domain.entity.Periodo;
 
-public interface PeriodGateway {
-
-    Periodo createPeriod(Periodo periodo);
-    Periodo updatePeriod(Periodo periodo);
-    void deletePeriod(Periodo periodo);
-    Periodo getPeriodById(int id);
-}
+public interface PeriodGateway extends Gateway<Periodo> {}
