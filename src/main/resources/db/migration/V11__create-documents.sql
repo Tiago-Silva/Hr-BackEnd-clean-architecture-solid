e52815a43be7d011e9e7ab5beb87842b583d6f09
Tiago <ti.almeida58@gmail.com>
2024-10-23 08:44:21 -0300

    refactor: renaming to English

---
+++ src/main/resources/db/migration/V11__create-documents.sql
@@ -0,0 +1,16 @@
+CREATE TABLE documents (
+    id INT AUTO_INCREMENT,
+    type VARCHAR(45),
+    document_url VARCHAR(45),
+    name VARCHAR(45),
+    description VARCHAR(45),
+    idemployee INT,
+    employee_name VARCHAR(45),
+    department VARCHAR(45),
+    enterprise_id INT,
+    date DATE,
+    absence_id INT,
+    employee_id INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (employee_id) REFERENCES employee(id)
+);
