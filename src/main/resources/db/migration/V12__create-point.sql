79eed598bd0bb228ee0e48454ceadc677d06abd5
Tiago <ti.almeida58@gmail.com>
2024-10-23 08:44:25 -0300

    refactor: renaming to English

---
+++ src/main/resources/db/migration/V12__create-point.sql
@@ -0,0 +1,21 @@
+CREATE TABLE point (
+    id INT AUTO_INCREMENT,
+    badge VARCHAR(45),
+    date DATE,
+    time TIME,
+    start_time TIME,
+    end_time TIME,
+    break_end TIME,
+    break_start TIME,
+    rep VARCHAR(45),
+    nsr VARCHAR(45),
+    pis VARCHAR(45),
+    employee_name VARCHAR(45),
+    employee_id INT,
+    cnpj VARCHAR(45),
+    enterprise_name VARCHAR(45),
+    late_start BOOLEAN,
+    enterprise_id INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (enterprise_id) REFERENCES enterprise(id)
+);
