556ff12e28c019e7c689190bd3122bbba6aa94ce
Tiago <ti.almeida58@gmail.com>
2024-10-23 08:43:04 -0300

    refactor: renaming to English

---
+++ src/main/resources/db/migration/V9__create-prior-notice.sql
@@ -0,0 +1,18 @@
+CREATE TABLE prior_notice (
+    id INT AUTO_INCREMENT,
+    reason VARCHAR(45),
+    description VARCHAR(45),
+    start_date DATE,
+    end_date DATE,
+    employee_name VARCHAR(45),
+    idemployee INT,
+    department VARCHAR(45),
+    enterprise_id INT,
+    return_date DATE,
+    reminder_id INT,
+    notice_type VARCHAR(45),
+    days VARCHAR(45),
+    employee_id INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (employee_id) REFERENCES employee(id)
+);
