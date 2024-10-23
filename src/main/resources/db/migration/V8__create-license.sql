7368f138e90d9ad88129b58cca18b9fcfc38ec0b
Tiago <ti.almeida58@gmail.com>
2024-10-23 08:43:01 -0300

    refactor: renaming to English

---
+++ src/main/resources/db/migration/V8__create-license.sql
@@ -0,0 +1,16 @@
+CREATE TABLE license (
+    id INT AUTO_INCREMENT,
+    type VARCHAR(45),
+    description VARCHAR(45),
+    start_date DATE,
+    end_date DATE,
+    return_date DATE,
+    employee_name VARCHAR(45),
+    idemployee INT,
+    department VARCHAR(45),
+    enterprise_id INT,
+    reminder_id INT,
+    employee_id INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (employee_id) REFERENCES employee(id)
+);
