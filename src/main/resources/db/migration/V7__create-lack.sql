326cbe91d19fe30929969eca5256614f8193972a
Tiago <ti.almeida58@gmail.com>
2024-10-23 08:42:58 -0300

    refactor: renaming to English

---
+++ src/main/resources/db/migration/V7__create-lack.sql
@@ -0,0 +1,16 @@
+CREATE TABLE lack (
+    id INT AUTO_INCREMENT,
+    start_date DATE,
+    end_date DATE,
+    reason VARCHAR(255),
+    description VARCHAR(255),
+    employee_name VARCHAR(255),
+    department VARCHAR(255),
+    return_date DATE,
+    enterprise_id INT,
+    idemployee INT,
+    reminder_id INT,
+    employee_id INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (employee_id) REFERENCES employee(id)
+);
